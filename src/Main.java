b19b02e7bc3aca7502ecc7a62e94f998d901b2d5
Baranova.K <waynaungelneal@gmail.com>
2025-04-23 16:57:41 +0300

    hw done

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,30 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Author rowling = new Author("Joanne", "Rowling ");
+        Author pushkin = new Author("Alexander", "Pushkin");
+        System.out.println("Имя - " + rowling.getName());
+        System.out.println("Фамилия - " + rowling.getSurname() + "\n");
+        System.out.println("Имя - " + pushkin.getName());
+        System.out.println("Фамилия - " + pushkin.getSurname() + "\n\n");
+
+        Book harryPotter = new Book("Harry Potter", rowling, 1997);
+        Book evgeniyOnegin = new Book("Evgeniy Onegin", pushkin, 1833);
+        System.out.println("Название - " + harryPotter.getTitle());
+        System.out.println("Автор - " + harryPotter.getAuthor().getName() + " " + harryPotter.getAuthor().getSurname());
+        System.out.println("Год публикации - " + harryPotter.getYear() + "\n");
+
+        System.out.println("Название - " + evgeniyOnegin.getTitle());
+        System.out.println("Автор - " + evgeniyOnegin.getAuthor().getName() + " " + evgeniyOnegin.getAuthor().getSurname());
+        System.out.println("Год публикации - " + evgeniyOnegin.getYear() + "\n");
+
+        evgeniyOnegin.setYear(2026);
+        System.out.println("Год публикации - " + evgeniyOnegin.getYear());
+
+
+
+
+
     }
 }
