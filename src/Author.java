5115fefc5c47ba8f00cfc94dfb7bafe5b4db5b2b
Baranova.K <waynaungelneal@gmail.com>
2025-04-29 11:45:34 +0300

    hw done

--- src/Author.java
+++ src/Author.java
@@ -7,6 +7,24 @@ public class Author {
         this.surname = surname;
     }
 
+    public String toString() {
+        return "Имя: " + this.name + ", Фамилия: " + this.surname;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this.getClass() != other.getClass()) {
+            return false;
+        }
+        Author author2 = (Author) other;
+        return (name.equals(author2.name) && surname.equals(author2.surname));
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(name, surname);
+    }
+
     public String getName() {
         return this.name;
     }
