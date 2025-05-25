69b52823867e94414cd7aa395842b5e431f92f66
ullqp <waynaungelneal@gmail.com>
2025-05-25 19:57:54 +0300

    Update ServiceStation.java

--- src/ServiceStation.java
+++ src/ServiceStation.java
@@ -9,21 +9,18 @@ public class ServiceStation {
     public void check(Bicycle bicycle) {
         if (bicycle == null) {return;}
         check((Vehicle) bicycle);
-        System.out.println("\n");
     }
 
     public void check(Car car) {
         if (car == null) {return;}
         check((Vehicle) car);
         car.checkEngine();
-        System.out.println("\n");
     }
 
     public void check(Truck truck) {
         if (truck == null) {return;}
         check((Car) truck);
         truck.checkTrailer();
-        System.out.println("\n");
     }
 
     public void check(Car car, Bicycle bicycle, Truck truck) {
